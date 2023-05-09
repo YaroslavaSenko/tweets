a6a1a0a8083260ff6f051ee27a009453e8bf267f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-09 22:54:19 +0300

    Update Home.styled.js

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -5,5 +5,18 @@ export const Container = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 28px 36px 36px;
+    width: 700px;
+       background: linear-gradient( 114.99deg, #471ca9 -0.99%, #5736a3 54.28%, #4b2a99 78.99% );
+    box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+    border-radius: 20px;
+
+  
+`;
+export const Text = styled.h1`
+ color: white;
   
 `;
