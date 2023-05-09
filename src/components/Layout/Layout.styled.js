b57d33b55a9fc635855780a57857381891d689eb
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-09 22:54:08 +0300

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -5,3 +5,23 @@ export const Container = styled.div`
   margin-right: auto;
   margin-left: auto;
 `;
+
+export const Navigation = styled.nav`
+ position: fixed;
+display: flex;
+gap: 30px;
+align-items: center;
+padding-left: 50px;
+height: 50px;
+text-decoration: none;
+background: linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);
+box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+font-size: large;
+font-weight: 700;
+color: #EBD8FF;
+width: 1200px;
+`;
+
+export const Text = styled.p`
+  color: white;
+`;
